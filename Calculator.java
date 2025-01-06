43834242d4dd5677d6ec1109a7abee08a8b4e2df
HP-Harshit <harshitpatel274@gmail.com>
2025-01-06 23:44:14 +0530

    Add UC2: Subtraction functionality

--- Calculator.java
+++ Calculator.java
@@ -5,18 +5,24 @@ public class Calculator {
         System.out.println("Welcome to Calculator");
         Scanner scanner = new Scanner(System.in);
         
-        System.out.println("Choose an operation: 1 for Addition");
+        System.out.println("Choose an operation: 1 for Addition, 2 for Subtraction");
         int choice = scanner.nextInt();
         
         System.out.println("Enter two numbers:");
         int num1 = scanner.nextInt();
         int num2 = scanner.nextInt();
 
-        if (choice == 1) {
-            int sum = num1 + num2;
-            System.out.println("The sum is: " + sum);
-        } else {
-            System.out.println("Invalid choice.");
+        switch (choice) {
+            case 1:
+                int sum = num1 + num2;
+                System.out.println("The sum is: " + sum);
+                break;
+            case 2:
+                int difference = num1 - num2;
+                System.out.println("The difference is: " + difference);
+                break;
+            default:
+                System.out.println("Invalid choice.");
         }
     }
 }
